c9199e9c776555bf2a234eaf2150ce98ab46f9db
Michal Majewski <m.majewski1185@gmail.com>
2018-04-07 18:30:08 +0200

    url change

--- BetterTwitter/urls.py
+++ BetterTwitter/urls.py
@@ -19,6 +19,6 @@ from MyTwitter import views
 
 urlpatterns = [
     url('^admin/', admin.site.urls),
-    url(r'^mytwitter/', include('MyTwitter.urls'))
+    # url(r'^mytwitter/', include('MyTwitter.urls'))
 
 ]
