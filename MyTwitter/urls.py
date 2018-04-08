fd78ecb199205d356067bde67fdb9ac9d07d65c8
Michal Majewski <m.majewski1185@gmail.com>
2018-04-08 12:32:56 +0200

    test
    Please enter the commit message for your changes. Lines starting

---
+++ MyTwitter/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from MyTwitter import views
+
+
+urlpatterns = [
+    url(r'^aaa$', ),
+
+
+
+]
--- MyTwitter/views.py
+++ MyTwitter/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+
+
+
+class SchoolView(View):
+
+    def get(self, request):
+
+        return render(request, "MyTwitter/base.html")
+
+
+
